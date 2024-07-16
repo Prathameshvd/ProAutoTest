a9cc81d211ce90427393f7f9a41d78a2e7700d0a
prathmesh donkar <prathameshdonkar28@gmail.com>
2024-07-16 20:55:55 +0300

    Pushing project basic folder structure

---
+++ src/test/java/com/runner/TestRunner.java
@@ -0,0 +1,17 @@
+package com.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "Feature"
+        ,glue={"stepDefinition"}
+)
+
+public class TestRunner {
+
+}
