cc00310f310bce0c1b2eb10486d2051b735dda23
prathmesh donkar <prathameshdonkar28@gmail.com>
2024-07-17 20:09:49 +0300

    Pushing basic project working code

---
+++ src/main/java/glueCodeImplementation/GlueCodeImplementation.java
@@ -0,0 +1,9 @@
+package glueCodeImplementation;
+
+public class GlueCodeImplementation {
+
+    public void SimpleMethod()
+    {
+        System.out.println("1st defination");
+    }
+}
--- src/test/java/com/runner/TestRunner.java
+++ src/test/java/com/runner/TestRunner.java
@@ -4,12 +4,11 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "Feature"
-        ,glue={"stepDefinition"}
+        features = "feature/Sample.feature"
+        ,glue={"glueCode"}
+        ,monochrome = false
 )
 
 public class TestRunner {
